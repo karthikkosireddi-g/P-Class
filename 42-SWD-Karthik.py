7901d6f74e5ebcf0fff3c2f7a18da1c11c4fce39
kk <kk@varkasa.com>
2019-12-05 12:06:18 -0800

    from KK - changed Laura's code

--- 42-SWD-Karthik.py
+++ 42-SWD-Karthik.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 
-myD1 = webdriver.Chrome()
-myD2 = webdriver.Firefox()
+myD1 = webdriver.Chrome(executable_path="C:\\Users\\jcady\\PycharmProjects\\chromedriver.exe")
 
-myD1.get("https://itelearn.com/")
-myD2.get("https://anyaut.com/")
+myD1.get("https://mortgagecalculator.org/")
+myD1.find_element_by_id("homeval").send_keys("433300")
+
+
+myD1.get("https://www.pse.com/")
+
+myD1.get("https://10.232.53.39/php/login.php")
---
+++ 43-SWD-PaloAltoNetworks.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+myD1 = webdriver.Chrome(executable_path="C:\\Users\\jcady\\PycharmProjects\\chromedriver.exe")
+
+vURL = "https://10.232.53.39/php/login.php"
+vUN = "p-class"
+vPWD = "Password01"
+
+myD1.get(vURL)
+myD1.find_element_by_id("user").send_keys(vUN)
+myD1.find_element_by_id("passwd").send_keys(vPWD)
+
+uNameLabel = "//*[@id='trInitName']/td[1]/label"
+
+# print (myD1.find_element_by_xpath(uNameLabel).get_attribute("text"))
+
+myD1.find_element_by_name("ok").click()
+
+# //*[@id="ext-gen93"]
